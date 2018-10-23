d238daf5aec359ccde5480c404120a20260b62e7
Constable <n_constable2@tss-hr420-dm18.fc.ca>
2018-10-23 13:08:57 -0400

    RpsGame+con added

---
+++ conditions.py
@@ -0,0 +1,10 @@
+# print a message to the terminal window
+print("Rules that govern the state of water")
+
+# set up a variable to hold the temp we input
+current_temp = False
+
+while current_temp is False:
+    current_temp = input("Enter a temperature:\n")
+    # see what current temp is
+    print(current_temp)
