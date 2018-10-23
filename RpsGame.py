62023c13fea42fa2f14d4cad7fd8d662a62fb511
Constable <n_constable2@tss-hr420-dm18.fc.ca>
2018-10-23 14:31:53 -0400

    set up game

--- RpsGame.py
+++ RpsGame.py
@@ -1,12 +1,53 @@
 # import the random packages so that we can generate random numbers
 from random import randint
 
-#choices is an array => a container that can hold multiple items 
+# choices is an array => a container that can hold multiple items
 # arrays are a 0-based -> the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
+player = False
 
-# make the computer choose a weapon from the choices array at random 
-computer_choice = choices [randint(0,2)]
+# make the computer choose a weapon from the choices array at random
+computer_choice = choices[randint(0, 2)]
 
 # print the choice to the terminal window
 print("Computer chooses: ", computer_choice)
+
+# set up our loop
+while player is False:
+    # set player to True by making a selection
+    print("choose your weapon!")
+    player = input("Rock, Paper or Scissors?")
+
+    print(player, "\n")
+
+    # check for a tie = choices are the same
+    if player == computer_choice:
+        print("Draw!")
+    # check to see if computer choice beats our choice or not
+    elif player == "Rock":
+        if computer_choice == "Paper":
+            # computer won, nooooooooo!
+            print("You lose! Paper covers Rock!")
+        else:
+            # we have won!
+            print("You win!")
+
+    elif player == "Paper":
+        if computer_choice == "Scissors":
+            print("You lose!", computer_choice, "cut", player)
+        else:
+            print("You won!", player, "covers", computer_choice)
+
+    elif player == "Scissors":
+        if computer_choice == "Rock":
+            print("You Lose", computer_choice, "smashes", player)
+        else:
+            print("You Win!", player, "cut", computer_choice)
+    elif player == "quit":
+        exit()
+    else:
+        print("Check your spelling... I don't get your human words\n")
+
+    # reset the game loop and start over again
+    player = False
+    computer_choice = choices[randint(0, 2)]
--- conditions.py
+++ conditions.py
@@ -5,6 +5,20 @@ print("Rules that govern the state of water")
 current_temp = False
 
 while current_temp is False:
-    current_temp = input("Enter a temperature:\n")
+    # MAKE THIS A NUMBER!!
+    x = current_temp
+    current_temp = x
     # see what current temp is
-    print(current_temp)
+    print("you input:", x)
+    # if the current temp is at freezing or below, water is solid
+    if (int(x) < 0 or (int(x) == 0)):
+        print("water is a solid! cuz it be all froze and shit")
+        x = False
+    # else check another condition, if it's not freezing, is it below boiling?
+    elif (int(x) < 100):
+        print ("water is a liquid, cuz it ain't freezing or boiling")
+        x = False
+
+    elif (int(x) > 100 or (int(x) == 100)):
+        print("water is a gas, cuz it be hot")
+        x = False
